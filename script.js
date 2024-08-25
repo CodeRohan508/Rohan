86c9d2df582d601a04c88f8f069c7e15cfb56b3d
CodeRohan508 <9r9h4n@keemail.me>
2024-08-25 16:28:19 +0600

    B

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function OpenSideBar() {
   document.querySelector(".circle").classList.add("open");
-  document.querySelector(".OpenSideBar").classList.add("SideHide")
-  document.querySelector(".CloseSideBar").classList.remove("SideHide")
+  document.querySelector(".OpenSideBar").classList.add("SideHide");
+  document.querySelector(".CloseSideBar").classList.remove("SideHide");
 }
 
 function CloseSideBar() {
   document.querySelector(".circle").classList.remove("open");
-  document.querySelector(".OpenSideBar").classList.remove("SideHide")
-  document.querySelector(".CloseSideBar").classList.add("SideHide")
+  document.querySelector(".OpenSideBar").classList.remove("SideHide");
+  document.querySelector(".CloseSideBar").classList.add("SideHide");
 }
 
 function openForm() {
-  document.querySelector(".form").classList.add("openForm")
-  document.querySelector(".bodyb").classList.add("actb")
+  document.querySelector(".form").classList.add("openForm");
+  document.querySelector(".bodyb").classList.add("actb");
 }
 
 function closeForm() {
-  document.querySelector(".form").classList.remove("openForm")
-  document.querySelector(".bodyb").classList.remove("actb")
+  document.querySelector(".form").classList.remove("openForm");
+  document.querySelector(".bodyb").classList.remove("actb");
 }
