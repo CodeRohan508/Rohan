6e171c575cb02f7073d47e46a802b307dd00df1d
CodeRohan508 <9r9h4n@keemail.me>
2024-09-07 15:11:03 +0600

    b

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     extend: {
       fontFamily: {
         sf: "SF Pro Display"
-      }
+      },
+      textShadow: {
+        sm: '1px 1px 2px rgba(0, 0, 0, 0.5)',  // Small shadow
+        md: '2px 2px 4px rgba(0, 0, 0, 0.5)',  // Medium shadow
+        lg: '3px 3px 6px rgba(0, 0, 0, 0.5)',  // Large shadow
+        xl: '4px 4px 8px rgba(0, 0, 0, 0.5)',  // Extra large shadow
+        none: 'none',  // No shadow
+      },
     },
   },
   plugins: [],
