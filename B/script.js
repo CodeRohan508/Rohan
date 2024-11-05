0a685ceda87a97c618cef9f4d8ce47245a1dca35
CodeRohan508 <9r9h4n@keemail.me>
2024-11-05 16:34:44 +0600

    b

--- B/script.js
+++ B/script.js
@@ -1,17 +1,25 @@
-const contextMenu = document.getElementById('context-menu');
-const rightClickArea = document.body
+const contextMenu = document.getElementById("context-menu");
+const rightClickArea = document.body;
 
-rightClickArea.addEventListener('contextmenu', (e) => {
+rightClickArea.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 
   const { clientX: mouseX, clientY: mouseY } = e;
   contextMenu.style.top = `${mouseY}px`;
   contextMenu.style.left = `${mouseX}px`;
-  contextMenu.style.display = 'block'; // Show the menu at the click location
-  contextMenu.classList.remove('hidden');
+  contextMenu.style.display = "block"; // Show the menu at the click location
+  contextMenu.classList.remove("hidden");
 });
 
-document.addEventListener('click', () => {
-  contextMenu.style.display = 'none'; // Hide the menu when clicking elsewhere
-  contextMenu.classList.add('hidden');
+document.addEventListener("click", () => {
+  contextMenu.style.display = "none"; // Hide the menu when clicking elsewhere
+  contextMenu.classList.add("hidden");
+});
+
+ScrollReveal().reveal("#skills", {
+  duration: 1000, // Animation duration in milliseconds
+  origin: "bottom", // Start position ('top', 'right', 'bottom', 'left')
+  distance: "50px", // Distance the element moves during the reveal
+  delay: 200, // Delay before the animation starts
+  reset: true, // Animation resets on scroll (optional)
 });
